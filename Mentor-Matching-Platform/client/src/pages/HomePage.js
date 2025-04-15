231d24684585e5254887e6c19f527bb3dbe35689
berry1104 <935311076@qq.com>
2025-04-15 18:44:48 +0800

    test tailwind css with homepage

--- Mentor-Matching-Platform/client/src/pages/HomePage.js
+++ Mentor-Matching-Platform/client/src/pages/HomePage.js
@@ -3,9 +3,11 @@ import React from "react";
 
 function HomePage() {
   return (
-    <div>
+    <div className="max-w-lg mx-auto p-6 bg-gray-100 rounded shadow">
       <h2>Welcome to the Mentor-Mentee Portal</h2>
       <p>This is the home page. Feel free to explore!</p>
+
+      <h1 className="text-2xl font-bold text-center">test tailinwindcss</h1>
     </div>
   );
 }
