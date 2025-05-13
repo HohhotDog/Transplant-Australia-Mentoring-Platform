9ddd86ed2999609d3fc8aad1d34bb75efacc46fc
berry1104 <935311076@qq.com>
2025-05-13 19:52:59 +0800

    change btn color

--- Mentor-Matching-Platform/client/src/pages/Mentorship/ApprovedSessionDetailPage.js
+++ Mentor-Matching-Platform/client/src/pages/Mentorship/ApprovedSessionDetailPage.js
@@ -9,16 +9,16 @@ const ApprovedSessionDetailPage = ({ session }) => {
   return (
 <div className="max-w-4xl mx-auto p-6">
       {/* Tabs */}
-<div className="flex space-x-6 border-b border-gray-300 mb-4">
+<div className="flex space-x-6 border-b  mb-4">
 <button
           onClick={() => setActiveTab('details')}
-          className={`pb-2 font-medium ${activeTab === 'details' ? 'border-b-2 border-black' : 'text-gray-500'}`}
+          className={`pb-2 bg-transparent font-medium ${activeTab === 'details' ? 'border-b-2 border-black text-btnorange' : 'text-black'}`}
 >
           Session Details
 </button>
 <button
           onClick={() => setActiveTab('connections')}
-          className={`pb-2 font-medium ${activeTab === 'connections' ? 'border-b-2 border-black' : 'text-gray-500'}`}
+          className={`pb-2 bg-transparent font-medium ${activeTab === 'connections' ? 'border-b-2 border-black text-btnorange' : 'text-black'}`}
 >
           My Connections
 </button>
