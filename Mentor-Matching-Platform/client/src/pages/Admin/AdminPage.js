351b1c857e5158622cde67c9bffede19e21dfd15
berry1104 <935311076@qq.com>
2025-05-15 13:22:46 +0800

    create session btn

--- Mentor-Matching-Platform/client/src/pages/Admin/AdminPage.js
+++ Mentor-Matching-Platform/client/src/pages/Admin/AdminPage.js
@@ -24,13 +24,22 @@ const AdminPage = () => {
         setLoading(false);
       });
   }, []);
-
+  
+  const handleCreate = () => {
+    // Navigate to the session creation page
+    // navigate('/admin/sessions/create');
+  
+  };
   if (loading) return <p className="p-4">Loading sessions...</p>;
   if (error) return <p className="p-4 text-red-500">Error: {error.message}</p>;
 
   return (
     <div className="p-6">
       <h2 className="text-2xl font-semibold mb-4">Admin Dashboard</h2>
+      <button
+          onClick={handleCreate}
+          className="px-4 py-2 bg-transparent hover:bg-btnorange text-black rounded shadow"
+        >Create New Sessions</button>
       <SessionTable sessions={sessions} />
     </div>
   );
